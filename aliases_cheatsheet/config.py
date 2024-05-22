7145aa01458c22e6393fcc25728c57c803369d23
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-05-22 13:29:19 +0200

    update config.py

--- aliases_cheatsheet/config.py
+++ aliases_cheatsheet/config.py
@@ -2,5 +2,11 @@ from pathlib import Path
 
 
 APP_DIR = Path(__file__).parent
-JSON_NAME = "aliases.json"
-JSON_PATH = APP_DIR / "www" / "assets" / "data" / JSON_NAME
+DATA_DIR = APP_DIR / "www" / "assets" / "data"
+
+# Output files.
+ALIASES_JSON_NAME = "aliases.json"
+ALIASES_JSON_PATH = DATA_DIR / ALIASES_JSON_NAME
+
+GIT_ALIASES_JSON_NAME = "git_aliases.json"
+GIT_ALIASES_JSON_PATH = DATA_DIR / GIT_ALIASES_JSON_NAME
